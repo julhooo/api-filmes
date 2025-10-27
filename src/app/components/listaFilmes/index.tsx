7708f4e2e2956fb9eaafc8a0cbb9c70342c5e197
júlio <julioreqfraga@gmail.com>
2025-10-27 02:44:18 -0300

    scroll infinito

--- src/app/components/listaFilmes/index.tsx
+++ src/app/components/listaFilmes/index.tsx
@@ -3,28 +3,48 @@
 import { useEffect, useState } from 'react';
 import './index.scss';
 import axios from 'axios';
-import { FilmeType } from '@/app/types';
+import { FilmeType } from '@/app/types/movie';
 import CardFilmes from '../CardFilmes';
 
 export default function ListaFilmes() {
     const [filmes, setFilmes] = useState<FilmeType[]>([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() =>{
-        getFilmes();
-    }, []);
+        getFilmes(page);
+    }, [page]);
 
-    const getFilmes = () => {
-        axios({
+    const getFilmes = async (pagina: number) => {
+
+        setLoading(true);
+        await axios({
             method: 'get',
             url: 'https://api.themoviedb.org/3/discover/movie',
             params: {
                 api_key: '',
-                language: 'pt-BR'
+                language: 'pt-BR',
+                page: pagina
             }
         }).then(response => {
-            setFilmes(response.data.results);
-        })
+            setFilmes((prev) => [...prev, ...response.data.results]);
+        });
+
+        setLoading(false);
     }
+ 
+    useEffect(() =>{
+        function handleScroll(){
+            if(
+                window.innerHeight + window.scrollY >= document.body.offsetHeight - 200 && !loading
+            ){
+                setPage((prev) => prev + 1);
+            }
+        }
+
+            window.addEventListener("scroll", handleScroll);
+            return() => window.removeEventListener("scroll", handleScroll);
+        }, [loading]);
 
 
     return (
@@ -35,6 +55,7 @@ export default function ListaFilmes() {
                     filme={filme}
                 />
             )}
+            {loading && <p> ... </p>}
         </ul>
     )
 }
